3468496ec2adbc51832e5789fe86ece07b7e46dd
Partha <168160706+Leoneix@users.noreply.github.com>
2026-02-14 19:51:28 +0530

    smooth scroll and nav stuff

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Portfolio Loaded Successfully");
+document.querySelectorAll("nav a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+
+    window.scrollTo({
+      top: target.offsetTop - 30,
+      behavior: "smooth",
+    });
+  });
+});
